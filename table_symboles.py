c43602c0d0541b7aa8ae994c47056e8c55df4725
komi-assimpah <115261320+komi-assimpah@users.noreply.github.com>
2023-06-11 23:53:45 +0200

    oh lala

--- table_symboles.py
+++ table_symboles.py
@@ -1,4 +1,17 @@
 class TableSymboles:
+    types = {
+        type(1): "entier",
+        type(True): "booleen"
+    }
+
     def __init__(self):
         self.symboles = {}
         
+    def verifier_exist(self, nom, type_args):
+        if nom in self.symboles:
+            if len(type_args) != len(self.symboles[nom]['args']):
+                return False
+
+            return [TableSymboles.types[i] for i in type_args] == self.symboles[nom]['args']
+        else:
+            return False
